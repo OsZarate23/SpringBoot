5dfcb375d59ea928d9247ccdf88c9de72f38add7
Osmar Zarate <osmarzarate@MacBook-Pro-de-Osmar.local>
2024-07-28 19:50:19 -0600

    Conexion SQL y Mongo

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -3,7 +3,9 @@ package com.example.demo;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+import com.spring.data.DemoApplication;
+
+@SpringBootTest(classes = DemoApplication.class)
 class DemoApplicationTests {
 
 	@Test
